74df40fd83900e525d199da4115b43c9bc2c45a1
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-05-26 18:19:42 -0700

    Add raml-praser example

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+var raml = require('raml-parser');
 
+ raml.loadFile('example.raml').then( function(data) {
+   console.log(data);
+ }, function(error) {
+   console.log('Error parsing: ' + error);
+ }).then( function() {
+
+  raml.composeFile('example.raml').then( function(rootNode) {
+      console.log('Root Node: ');
+      console.log(rootNode)
+    }, function(error) {
+      console.log('Error parsing: ' + error);
+  })
+});
 
 module.exports = function() {
 
-  var raml = require('raml-parser');
-
-   raml.loadFile('example.raml').then( function(data) {
-     console.log(data);
-   }, function(error) {
-     console.log('Error parsing: ' + error);
-   });
 
 };
