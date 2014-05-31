9d57224340ef139b600e96008b4f95bc1545b0d8
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-05-31 15:40:28 -0700

    Restructure code a bit

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
+#!/usr/bin/env node
+
+'use strict';
+
 var raml = require('raml-parser');
+var yaml = require('js-yaml');
+var fs = require('fs');
 
- raml.loadFile('example.raml').then( function(data) {
-   console.log(data);
- }, function(error) {
+function emit(ramlObject) {
+  var result = "#%RAML 0.8\n" + yaml.dump(ramlObject);
+
+  console.log(ramlObject);
+
+  console.log();
+
+  // if not run in browser
+  if (! window) {
+    fs.writeFile("output.raml", result, function(err) {
+      if(err) {
+          console.log(err);
+      } else {
+          console.log("The file was saved!");
+      }
+    });
+  }
+
+  // document.body.innerHTML = '<pre>'+ JSON.stringify(data, null, 2) + '</pre>';
+  document.body.innerHTML = '<pre>'+ result + '</pre>';
+}
+
+raml.loadFile('example.raml').then(
+  function (data) {
+    emit(data);
+  },
+  function(error) {
    console.log('Error parsing: ' + error);
- }).then( function() {
-
-  raml.composeFile('example.raml').then( function(rootNode) {
-      console.log('Root Node: ');
-      console.log(rootNode)
-    }, function(error) {
-      console.log('Error parsing: ' + error);
-  })
-});
-
-module.exports = function() {
+  });
 
 
-};
+
+module.exports.emit = emit;
