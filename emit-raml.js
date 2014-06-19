16309734e0a36d228176d92112e12c48366ebe5e
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-06-19 12:40:52 -0700

    Go back to parsing ramlObject as string and add collection of ramls for future testing and

--- emit-raml.js
+++ emit-raml.js
@@ -3,36 +3,13 @@
 'use strict';
 
 var raml = require('raml-parser');
-var raml2html = require('raml2html');
 var fs = require('fs');
 
 function emit(ramlObject) {
-  var template = require('./templates/template.handlebars');
-  var resourceTemplate = require('./templates/resource.handlebars');
-  var config = {
-      'template': template,
-      'partials': {
-          'resource': resourceTemplate
-      }
-  };
+  var result = "";
+  result = JSON.stringify(ramlObject, null, 2);
 
-  raml2html.parseWithConfig(ramlObject, config, function(result) {
-      console.log(result);
-
-      fs.writeFile("output.raml", result, function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("The file was saved!");
-        }
-      });
-
-      return result;
-  }, function(error) {
-      console.log('Error parsing: ' + error);
-      process.exit(1);
-  });
-
+  return result;
 }
 
 raml.loadFile('example.raml').then(
