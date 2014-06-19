505ea1b79516cefbe3cc44c6e710a31ff2ee5537
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-06-19 13:32:32 -0700

    Update npm start and npm test commands to actually work

--- test/test.js
+++ test/test.js
@@ -13,4 +13,16 @@ var TESTS = [
 ];
 
 describe('emit-raml', function () {
+  it('should work', function() {
+    assert.equal(true, true);
+  });
 });
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
